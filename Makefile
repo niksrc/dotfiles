d517812df51d00f488bc68ef35789e984a97b465
Nikhil Srivastava <niksrc@gmail.com>
2025-02-09 20:50:49 +0530

    updates

--- Makefile
+++ Makefile
@@ -1,21 +1,41 @@
 SHELL := bash
 
-.PHONY: dotfiles
+.PHONY: all
+all: etc dotfiles bin
 
-## Installs dotfiles.
-dotfiles: 
-	for file in $(shell find $(CURDIR) -name ".*" -not -name ".gitignore" -not -name ".git" -not -name ".config" -not name -not -name ".github" -not -name ".*.swp"); do \
+.PHONY: bin
+bin: ## Installs the bin directory files.
+	# add aliases for things in bin
+	for file in $(shell find $(CURDIR)/bin -type f -not -name "*-backlight" -not -name ".*.swp"); do \
 		f=$$(basename $$file); \
-		ln -sfn $$file $(HOME)/$$f; \
-	done; \
-	ln -fn $(CURDIR)/gitignore $(HOME)/.gitignore;
+		sudo ln -sf $$file /usr/local/bin/$$f; \
+	done
+
+.PHONY: dotfiles 
+dotfiles: ## Installs dotfiles.
+	stow git --target=$(HOME) --dotfiles
+	stow .config --target=$(HOME)/.config
+	stow --verbose  bash --target=$(HOME)
 	
-	mkdir -p $(HOME)/.local/share;	
+	mkdir -p $(HOME)/.local/share;
 	ln -snf $(CURDIR)/.fonts $(HOME)/.local/share/fonts;
 	fc-cache -f -v || true
-	
-	mkdir -p $(HOME)/.config;
-	ln -snf $(CURDIR)/.config/i3 $(HOME)/.config/i3
 
+.PHONY: etc 
+etc: ## Installs etc configs
+	sudo stow --verbose  etc --target=/etc
+	systemctl --user daemon-reload || true
+	sudo systemctl daemon-reload
+	sudo systemctl enable systemd-networkd systemd-resolved
+	sudo systemctl start systemd-networkd systemd-resolved
 
+.PHONY: test
+test: shellcheck
 
+.PHONY: shellcheck
+shellcheck:
+	./test.sh
+
+.PHONY: help
+help:
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
---
+++ bin/install.sh
@@ -0,0 +1,219 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+# install.sh
+#	This script installs my basic setup for a debian laptop
+
+export DEBIAN_FRONTEND=noninteractive
+
+check_is_sudo() {
+	if [ "$EUID" -ne 0 ]; then
+		echo "Please run as root."
+		exit
+	fi
+}
+
+
+setup_sources() {
+	apt update || true
+	apt install -y \
+		apt-transport-https \
+		ca-certificates \
+		curl \
+		dirmngr \
+		gnupg2 \
+		lsb-release \
+		--no-install-recommends
+
+	# turn off translations, speed up apt update
+	mkdir -p /etc/apt/apt.conf.d
+	echo 'Acquire::Languages "none";' > /etc/apt/apt.conf.d/99translations
+
+	# Add the Google Chrome distribution URI as a package source
+	cat <<-EOF > /etc/apt/sources.list.d/google-chrome.list
+	deb [arch=amd64 signed-by=/usr/share/keyrings/google.gpg] http://dl.google.com/linux/chrome/deb/ stable main
+	EOF
+
+	# Import the Google Chrome public key
+	curl https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor | sudo tee /usr/share/keyrings/google.gpg >/dev/null
+}
+
+base() {
+	apt update || true
+	apt -y upgrade
+
+	apt install -y \
+		ca-certificates \
+		git \
+		tzdata \
+		bash-completion \
+		coreutils \
+		dnsutils \
+		findutils \
+		net-tools \
+		file \
+		bc \
+		jq \
+		fzf \
+		silversearcher-ag \
+		shellcheck \
+		automake \
+		cmake \
+		build-essential \
+		strace \
+		tar \
+		bzip2 \
+		7zip \
+		gzip \
+		indent \
+		unzip \
+		xz-utils \
+		zip \
+		openssh-client \
+		openssh-server \
+		vim \
+		stow \
+		--no-install-recommends
+
+	apt autoremove -y
+	apt autoclean -y
+	apt clean -y
+}
+
+# install rust
+
+install_rust() {
+	curl https://sh.rustup.rs -sSf | sh
+	# Install clippy
+	rustup component add clippy
+}
+
+# install/update golang from source
+install_golang() {
+	export GO_VERSION
+	GO_VERSION=$(curl -sSL "https://go.dev/VERSION?m=text" | awk -Fgo '{ print $2 }')
+	export GO_SRC=/usr/local/go
+
+	# if we are passing the version
+	if [[ -n "$1" ]]; then
+		GO_VERSION=$1
+	fi
+
+	# purge old src
+	if [[ -d "$GO_SRC" ]]; then
+		sudo rm -rf "$GO_SRC"
+		sudo rm -rf "$GOPATH"
+	fi
+
+	GO_VERSION=${GO_VERSION#go}
+
+	# subshell
+	(
+	kernel=$(uname -s | tr '[:upper:]' '[:lower:]')
+	curl -sSL "https://storage.googleapis.com/golang/go${GO_VERSION}.${kernel}-amd64.tar.gz" | sudo tar -v -C /usr/local -xz
+	)
+}
+
+# install custom scripts/binaries
+install_scripts() {
+	# install speedtest
+	curl -sSL https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py  > /usr/local/bin/speedtest
+	chmod +x /usr/local/bin/speedtest
+
+	# install lolcat
+	curl -sSL https://raw.githubusercontent.com/tehmaze/lolcat/master/lolcat > /usr/local/bin/lolcat
+	chmod +x /usr/local/bin/lolcat
+}
+
+# install stuff for i3 window manager
+install_wmapps() {
+	sudo apt update || true
+	sudo apt install -y \
+		bluez \
+		bluez-firmware \
+		feh \
+		google-chrome-stable \
+		i3 \
+		i3lock \
+		i3status \
+		pulseaudio \
+		pulseaudio-module-bluetooth \
+		pulsemixer \
+		rofi \
+		rxvt-unicode \
+		scrot \
+		usbmuxd \
+		xclip \
+		xcompmgr \
+		--no-install-recommends
+
+	# start and enable pulseaudio
+	systemctl --user daemon-reload
+	systemctl --user enable pulseaudio.service
+	systemctl --user enable pulseaudio.socket
+	systemctl --user start pulseaudio.service
+
+	echo "Fonts file setup successfully now run:"
+	echo "	dpkg-reconfigure fontconfig-config"
+	echo "with settings: "
+	echo "	Autohinter, Automatic, No."
+	echo "Run: "
+	echo "	dpkg-reconfigure fontconfig"
+}
+
+install_tools() {
+	echo "Installing golang..."
+	echo
+	install_golang;
+
+	echo
+	echo "Installing rust..."
+	echo
+	install_rust;
+
+	echo
+	echo "Installing scripts..."
+	echo
+	sudo install.sh scripts;
+}
+
+usage() {
+	echo -e "install.sh\\n\\tThis script installs my basic setup for a debian laptop\\n"
+	echo "Usage:"
+	echo "  base                                - setup sources & install base pkgs"
+	echo "  wm                                  - install window manager/desktop pkgs"
+	echo "  golang                              - install golang and packages"
+	echo "  rust                                - install rust"
+	echo "  scripts                             - install scripts"
+	echo "  tools                               - install golang, rust, and scripts"
+}
+
+main() {
+	local cmd=$1
+
+	if [[ -z "$cmd" ]]; then
+		usage
+		exit 1
+	fi
+
+	if [[ $cmd == "base" ]]; then
+		check_is_sudo
+		setup_sources
+		base
+	elif [[ $cmd == "wm" ]]; then
+		install_wmapps
+	elif [[ $cmd == "rust" ]]; then
+		install_rust
+	elif [[ $cmd == "golang" ]]; then
+		install_golang "$2"
+	elif [[ $cmd == "scripts" ]]; then
+		install_scripts
+	elif [[ $cmd == "tools" ]]; then
+		install_tools
+	else
+		usage
+	fi
+}
+
+main "$@"
